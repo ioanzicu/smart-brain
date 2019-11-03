de502b838b8f1c6503f7a698f9201b1983eb0e87
Ioan Zicu <ioan.zicu94@gmail.com>
2019-11-03 01:13:50 +0100

    Profile Dropdown

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const Footer = () => (
-    <div className='center ma'>
-      <footer className='footer'>
-        <p>Created with <span style={{color: 'red'}}>&#10084;</span> by Ioan Zîcu</p>
-      </footer>
-    </div>
-  )
-
+  <div className='center mt-4'>
+    <footer className='footer'>
+      <p>
+        Created with <span style={{ color: 'red' }}>&#10084;</span> by Ioan Zîcu
+      </p>
+    </footer>
+  </div>
+)
 
 export default Footer
--- src/profile/ProfileIcon.js
+++ src/profile/ProfileIcon.js
@@ -1,14 +1,38 @@
 import React, { useState } from 'react'
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap'
 
 const ProfileIcon = props => {
   const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const toggle = () => setDropdownOpen(prevState => !prevState)
+
   return (
-    <div class='pa4 tc'>
-      <img
-        src='http://tachyons.io/img/logo.jpg'
-        class='br-100 h3 w3 dib'
-        alt='avatar'
-      />
+    <div className='pa4 tc'>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle
+          tag='span'
+          data-toggle='dropdown'
+          aria-expanded={dropdownOpen}>
+          <img
+            src='http://tachyons.io/img/logo.jpg'
+            className='br-100 h3 w3 dib'
+            alt='avatar'
+          />
+        </DropdownToggle>
+        <DropdownMenu
+          className='b--transparent shadow-5'
+          style={{
+            backgroundColor: 'rgba(255, 255, 255, 0.5)'
+          }}>
+          <DropdownItem header>View Profile</DropdownItem>
+          <DropdownItem>Sign Out</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   )
 }
